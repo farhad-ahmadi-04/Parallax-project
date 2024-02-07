e1f3896d6b897243f3f09badbf10dab964de6160
fari-panisher <farhad.13ahmadi.83@gmail.com>
2024-02-07 11:32:56 +0330

    set up folder & file
    Des :
    set up folder & file 🤩

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * This project includes several sections, in each section you will see different examples of parallax.
+ * Date : Wed Feb 07 2024 11:28:43 GMT+0330 (Iran Standard Time)
+ * owner : Farhad Ahmadi
+ * References : https://www.w3schools.com/howto/howto_css_parallax.asp
+ */
+// Variables
+
+// events
+
+// function
