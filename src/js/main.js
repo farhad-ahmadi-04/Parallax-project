09d543572dd64e51291556bf6bafeba60b72f119
fari-panisher <farhad.13ahmadi.83@gmail.com>
2024-02-10 17:52:35 +0330

    feat (landing page)
    Des :
    this demo version.
    + add images
    + add header and 2 sections
    + add css file
    + add units and varibles for sass
    * using strict for have modern codes in js
    + add function for handel header when user scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,27 @@
 /**
+ * 'use strict' => It is not a statement, but a literal expression, ignored by earlier versions of JavaScript.
+The purpose of "use strict"
+is to indicate that the code should be executed in "strict mode".
+With strict mode, you can not,
+for example, use undeclared variables.
+ */
+"use strict";
+/**
  * This project includes several sections, in each section you will see different examples of parallax.
  * Date : Wed Feb 07 2024 11:28:43 GMT+0330 (Iran Standard Time)
  * owner : Farhad Ahmadi
  * References : https://www.w3schools.com/howto/howto_css_parallax.asp
  */
 // Variables
-
+let section_Count = document.querySelector(".section-count>li:first-of-type");
+const header = document.querySelector("header");
+const text = document.querySelector(".parallax>span")
 // events
-
+window.addEventListener("scroll", srolling)
 // function
+function srolling(e) {
+    if (window.scrollY > 0) {
+        header.style.top = (window.scrollY * (-1)) + "px"
+        // text.style.top = ((window.scrollY - window.scrollY) + 500) + "px"
+    }
+}
